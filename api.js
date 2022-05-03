f507d323622e51e9cdb15455c64a2f9b529bbb19
Afonso Gomes de Amorim <afonso.amorim2014@hotmail.com>
2022-05-02 22:07:41 -0300

    finalizando projeto

--- api.js
+++ api.js
@@ -1,12 +1,13 @@
+function geraMensagem(){
 fetch("https://api.adviceslip.com/advice?")
 .then((resposta) => resposta.json())
 .then((data) => {console.log(data.slip.advice)
 
- document.getElementById("id").innerHTML = data.slip.id
+ //document.getElementById("id").innerHTML = data.slip.id
  document.getElementById("texto").innerHTML = data.slip.advice
 });
 
-
+};
 
 
 
