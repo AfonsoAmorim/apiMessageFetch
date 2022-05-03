4c7998590701c5929317590f64c80ac8e9894fcb
Afonso Gomes de Amorim <afonso.amorim2014@hotmail.com>
2022-05-02 21:18:22 -0300

    adicionando bootstrap

--- api.js
+++ api.js
@@ -1,8 +1,12 @@
 fetch("https://api.adviceslip.com/advice?")
 .then((resposta) => resposta.json())
 .then((data) => {console.log(data.slip.advice)
- document.getElementById("t").innerHTML = data.slip.advice
+
+ document.getElementById("id").innerHTML = data.slip.id
+ document.getElementById("texto").innerHTML = data.slip.advice
 });
+
+
 
 
 
